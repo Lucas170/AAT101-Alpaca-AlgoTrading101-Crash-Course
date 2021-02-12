293ceb587612d5fe7db6874796454d4045689115
Lucas Liew <lucasliew7@gmail.com>
2021-02-13 01:41:42 +0800

    initial commit

---
+++ tempx/temp2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:41:06 2021
+
+@author: liewl
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:17:36 2021
+
+@author: liewl
+"""
+import logging
+
+temp1 = 'temp1 print'
+TEMP2
+TEMP3 = 'temp1 print'
+
+print(temp1)
+
+print(TEMP3)
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+
+logging.info(temp1)
+
+logging.info(TEMP3)
