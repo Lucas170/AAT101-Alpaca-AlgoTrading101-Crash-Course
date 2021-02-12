ea7a88a26a635623aa9c34bb6a880e19b2373027
Lucas Liew <lucasliew7@gmail.com>
2021-02-13 01:47:28 +0800

    initial commit

---
+++ tempx/temp3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:41:06 2021
+
+@author: liewl
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:17:36 2021
+
+@author: liewl
+"""
+import logging
+
+temp1 = 'temp1 print'
+TEMP3 = 'temp3 print'
+
+print(temp1)
+
+print(TEMP3)
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+
+logging.info(temp1)
+
+logging.info(TEMP3)
+
+
+
+print(f'Order size limit of {temp1} reached.')
+print(f'Order size limit of {TEMP3} reached.')
