a9c02a892332c82ce531bf387dfda5730f3ed6df
Lucas Liew <lucasliew7@gmail.com>
2021-02-16 03:57:19 +0800

    Initial commit

---
+++ tempx/temp7.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:41:06 2021
+
+@author: liewl
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 01:17:36 2021
+
+@author: liewl
+"""
+
+import time 
+
+counter = 1
+
+while True:
+    print(counter)
+    counter += 1
+    time.sleep(3)
